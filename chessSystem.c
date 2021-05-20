15639068d6fa8b0a0e3cd7a97e6937a37c2c4c72
AssafAlon1 <alon.assaf@campus.technion.ac.il>
2021-05-20 17:15:04 +0300

    updated source files csl3

--- chessSystem.c
+++ chessSystem.c
@@ -3,3 +3,41 @@
 #include "tournament.h"
 #include "game.h"
 #include "player.h"
+
+
+
+struct chess_system_t {
+
+} ;
+
+
+//ChessSystem chessCreate()
+
+
+//void chessDestroy(ChessSystem chess)
+
+
+//ChessResult chessAddTournament (ChessSystem chess, int tournament_id,
+//                                int max_games_per_player, const char* tournament_location)
+
+
+//ChessResult chessAddGame(ChessSystem chess, int tournament_id, int first_player,
+//                         int second_player, Winner winner, int play_time)
+
+
+//ChessResult chessRemoveTournament (ChessSystem chess, int tournament_id)
+
+
+//ChessResult chessRemovePlayer(ChessSystem chess, int player_id)
+
+
+//ChessResult chessEndTournament (ChessSystem chess, int tournament_id)
+
+
+//double chessCalculateAveragePlayTime (ChessSystem chess, int player_id, ChessResult* chess_result)
+
+
+//ChessResult chessSavePlayersLevels (ChessSystem chess, FILE* file)
+
+
+//ChessResult chessSaveTournamentStatistics (ChessSystem chess, char* path_file)
